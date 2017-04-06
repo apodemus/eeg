e77e4c32eae34da8df69bc5f64489b1ff36522f2
apodemus <hannes@saao.ac.za>
2017-04-06 13:53:57 +0200

    starting

---
+++ io/load.py
@@ -0,0 +1,19 @@
+'''
+Convenient data selection, iteration and loading
+'''
+
+import numpy as np
+from scipy.io import loadmat
+
+
+#====================================================================================================
+def load_data(filename):
+    struct = loadmat(str(filename))
+    data, *stuff = struct['dataStruct'][0][0]
+    sampling_frq, length, chnls, *timestamp = \
+        (a.squeeze() if a.size > 1 else np.asscalar(a) for a in stuff)
+    duration = length / sampling_frq
+    Nch = len(chnls)
+    timestamp = int(timestamp[0]) if len(timestamp) else None
+    return data, duration, sampling_frq, Nch, timestamp
+
